28c6b5134b9bc69085425d8a8ed43988d8db9c09
fonCki <alfonsoridao@gmail.com>
2022-08-07 17:39:25 +0200

    CLI working, create new line, response with ArrowUp, ArrowDown, Execute commands

---
+++ Term2/Services/Command.cs
@@ -0,0 +1,20 @@
+using Term2.Shared;
+
+namespace Term2.Services; 
+
+public class Command {
+     public string Name { get; set; }
+     public string Response { get; set; }
+     public string Link { get; set; }
+     
+     public Command(string name, string response, string link) {
+          Name = name;
+          Response = response;
+          Link = link;
+     }
+     
+     public Command(string command) {
+         Response = ($"Command not found: {command}. Try 'help' to get started.");
+     }
+     
+}
--- Term2/Services/Commands.cs
+++ Term2/Services/Commands.cs
@@ -1,37 +1,64 @@
+using System.Runtime.CompilerServices;
 using System.Text;
+using static Term2.Services.Command;
 
 namespace Term2.Services; 
 
+
 public class Commands {
-    public bool Execute(string command) {
-        switch (command.ToLower()) {
-            case "instagram": {
-                                RunInstagram();
-                                break;
-            }
-            case "linkedin": {
-                                RunLinkedin();
-                                break;
-            }
-            case "email": {
-                                RunEmail();
-                                break;
-            }
-            default: return false;
-        }
+    public readonly List<Command?> _commandList = new List<Command?>();
 
-        return true;
-    }
-
-    private void RunEmail() {
-        Console.WriteLine("email - BABBBB");
-    }
-
-    private void RunLinkedin() {
-        Console.WriteLine("linkedin - BABBBB");
-    }
-
-    private void RunInstagram() {
-        Console.WriteLine("instagram - BABBBB");
+    public Commands() {
+        
+        Command? about = new Command("about",
+            "the response about About",
+            "");
+        
+        Command? banner = new Command("banner",
+            "the response about banner",
+            "");
+        
+        Command? clear = new Command("clear",
+            "the response about clear",
+            "");
+        
+        Command? date = new Command("date",
+            "the response about date",
+            "");
+        
+        Command? email = new Command("email",
+            "the response about email",
+            "mailto:alfonso@ridao.ar");
+        
+        Command? github = new Command("github",
+            "the response about github",
+            "https://github.com/fonCki");
+        
+        Command? help = new Command("help",
+            "the response about help",
+            "");
+        
+        Command? instagram = new Command("instagram",
+            "the response about instagram",
+            "https://www.instagram.com/alfonsoridao/");
+        
+        Command? linkedin = new Command("linkedin",
+            "the response about linkedin",
+            "https://www.linkedin.com/in/alfonsoridao");
+        
+        Command? empty = new Command("",
+            "",
+            "");
+        
+        _commandList.Add(about);
+        _commandList.Add(banner);
+        _commandList.Add(clear);
+        _commandList.Add(date);
+        _commandList.Add(email);
+        _commandList.Add(github);
+        _commandList.Add(help);
+        _commandList.Add(instagram);
+        _commandList.Add(linkedin);
+        _commandList.Add(empty);
     }
 }
