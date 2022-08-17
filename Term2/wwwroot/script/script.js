12f345d1aa6874fc49a6051bf563f32f77b7e164
fonCki <alfonsoridao@gmail.com>
2022-08-17 15:32:19 +0200

    tab keypress added

--- Term2/wwwroot/script/script.js
+++ Term2/wwwroot/script/script.js
@@ -1,5 +1,7 @@
-document.addEventListener('onkeypress', function (e) {
-    console.log(e);
+document.addEventListener('keydown', function (e) {
+    if (e.keyCode == 9) {  
+        e.preventDefault();
+    }
 });
 
 
@@ -7,32 +9,6 @@ function focusInput(id) {
     document.getElementById(id).focus();
 }
 
-// function OnScrollEvent(id) {
-//     document.documentElement.scrollTop = 100;
-// }
-
 function OnScrollEvent() {
     window.scrollTo(0,document.body.scrollHeight);
-}
-
-function invokeTabKey() {
-    // get the active element when Enter was pressed and 
-    // if it is an input, focus the next input
-    // NOTE: You cannot really trigger the browser event -
-    //       even if you do, the browser won't execute the action 
-    //       (such as focusing the next input) so you have to define the action
-    var currInput = document.activeElement;
-    if (currInput.tagName.toLowerCase() == "input") {
-        var inputs = document.getElementsByTagName("input");
-        var currInput = document.activeElement;
-        for (var i = 0; i < inputs.length; i++) {
-            if (inputs[i] == currInput) {
-                var next = inputs[i + 1];
-                if (next && next.focus) {
-                    next.focus();
-                }
-                break;
-            }
-        }
-    }
 }
