b0ed55f08aba849db75dda04271ac0df86d37c02
fonCki <alfonsoridao@gmail.com>
2022-08-17 16:05:21 +0200

    all files

---
+++ Term2/Model/Event.cs
@@ -0,0 +1,13 @@
+using System.Text.Json.Serialization;
+
+namespace Term2.Model; 
+
+public class Event {
+    public class Wiki {
+        public string Title { get; set; }
+        public string Wikipedia { get; set; }
+    }
+    public string Year { get; set; }
+    public string Description { get; set; }
+    public Wiki[] Wikipedia { get; set; }
+}
